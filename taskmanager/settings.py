0dc4199e5911c9f1937f114965cd4d8af1d48d22
EuReinoso <lucas.reinoso.br@gmail.com>
2021-11-16 21:22:34 -0300

    Heroku config

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,3 +159,7 @@ ACCOUNT_USERNAME_REQUIRED = True
 # django crispy-forms
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# django-heroku
+
+django_heroku.settings(locals())
